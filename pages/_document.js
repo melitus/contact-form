d07da58cd43f00d274e50eabd8c054fa8ce4e5db
melitus <asmelitus@gmail.com>
2021-03-11 17:11:38 +0100

    feat: add background image from pixels.com

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -9,35 +9,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Contact us</h1>
-      <p>lorem epsum</p>
+      <p className="sub-title">lorem epsum</p>
       <div id="contact-container">
         <div className="contact-info">
         <h4>Contact Information</h4>
           <p>Fill up the form and our team will get back to you in the next 24hours</p>
           <div className="icon-text">
-             <i class="fa fa-phone" aria-hidden="true"></i>
+             <i className="fa fa-phone" aria-hidden="true"></i>
             <span>+2348061329356</span>
           </div>
           <div className="icon-text">
-              <i class="fa fa-envelope-o" aria-hidden="true"></i>
+              <i className="fa fa-envelope-o" aria-hidden="true"></i>
             <span>asmelitus@gmail.com</span>
           </div>
           <div className="icon-text">
-             <i class="fa fa-map-marker" aria-hidden="true"></i>
+             <i className="fa fa-map-marker" aria-hidden="true"></i>
             <span>Lagos, Nigeria</span>
           </div>
           <div className="social-media">
            <a href="#" className="icon-circle">
-             <i class="fa fa-facebook-official" aria-hidden="true"></i>
+             <i className="fa fa-facebook-official" aria-hidden="true"></i>
            </a>
             <a href="#" className="icon-circle">
-             <i class="fa fa-twitter" aria-hidden="true"></i>
+             <i className="fa fa-twitter" aria-hidden="true"></i>
            </a>
             <a href="#" className="icon-circle">
-             <i class="fa fa-instagram" aria-hidden="true"></i>
+             <i className="fa fa-instagram" aria-hidden="true"></i>
            </a>
             <a href="#" className="icon-circle">
-             <i class="fa fa-youtube" aria-hidden="true"></i>
+             <i className="fa fa-youtube" aria-hidden="true"></i>
            </a>
           </div>
           </div>
